e698343d21e26da0fc6bb1064dca0c50a4fe7b13
JosephIth <josep.munozs@duocuc.cl>
2025-06-30 20:37:32 -0400

    Delete microServicioFactura directory

--- microServicioFactura/microServicioFactura/src/main/java/com/microServicioFactura/microServicioFactura/repository/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microServicioFactura.microServicioFactura.repository;
-
-import com.microServicioFactura.microServicioFactura.model.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
-}
--- microServicioFactura/microServicioFactura/src/main/java/com/microServicioFactura/microServicioFactura/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microServicioFactura.microServicioFactura.repository;
-
-import com.microServicioFactura.microServicioFactura.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Producto, Integer> {
-}
